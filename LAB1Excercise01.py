1819120f3409b702f809f9d0576dd124f606f478
Sjm168 <sju96@ku.edu>
2024-09-18 05:06:52 -0500

    Add files via upload

---
+++ LAB1Excercise01.py
@@ -0,0 +1,10 @@
+'''
+Author: SUNGMIN JU
+KUID: 3111697
+Date: 09/11/2024
+Lab: lab01
+Last modified: 09/16/2024
+Purpose: first lab exercise
+'''
+
+print("This is my first lab exercise!")
---
+++ LAB1Exercise02.py
@@ -0,0 +1,13 @@
+'''
+Author: SUNGMIN JU
+KUID: 3111697
+Date: 09/11/2024
+Lab: lab01
+Last modified: 09/16/2024
+Purpose: Display name, major, and hobbies
+'''
+
+print("My name is SUNGMIN JU.")
+print("I am an EECS major.")
+print("My hobbies are:BASKETBALL and Cooking")
+print("Goodbye")
